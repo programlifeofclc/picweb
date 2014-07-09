79ed3028490536860fc7c7072bc4dd562d200888
caolc <592292668@qq.com>
2014-07-09 18:21:35 +0800

    1

---
+++ tuweb/src/beans/ImgTheme.java
@@ -0,0 +1,63 @@
+package beans;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/** @author Hibernate CodeGenerator */
+public class ImgTheme implements Serializable {
+
+    /** identifier field */
+    private Long id;
+
+    /** persistent field */
+    private String themeName;
+
+    /** full constructor */
+    public ImgTheme(String themeName) {
+        this.themeName = themeName;
+    }
+
+    /** default constructor */
+    public ImgTheme() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getThemeName() {
+        return this.themeName;
+    }
+
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("id", getId())
+            .toString();
+    }
+
+    public boolean equals(Object other) {
+        if ( !(other instanceof ImgTheme) ) return false;
+        ImgTheme castOther = (ImgTheme) other;
+        return new EqualsBuilder()
+            .append(this.getId(), castOther.getId())
+            .isEquals();
+    }
+
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(getId())
+            .toHashCode();
+    }
+
+}
